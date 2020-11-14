f8382d8accbce821fe1970530f0a6d03bfd9ca5e
Andrey Pashkin <druidan@mail.ru>
2020-11-14 20:53:12 +0300

    Сделан график законченности работ по проекту

--- src/pages/Marketers/Marketers.tsx
+++ src/pages/Marketers/Marketers.tsx
@@ -9,18 +9,22 @@ import am4langRu from "@amcharts/amcharts4/lang/ru_RU";
 
 am4core.useTheme(am4themesAnimated);
 
+const data3: number [] = [0,0,0,1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,4,4,5,5,5,5,5,5,6,6,6,6,6,6,7,7,7,7,7,7,8,8,8,8,8,8,9,9,9,9,9,9,10,10,10,10,10,10,11,11,11,11,11,11,12,12,12,12,12,12,13,13,13,13,13,13,14,14,14,14,14,14,15,15,15,15,15,15,16,16,16,16,16,16,17,17,17,17,17,17,18,18,18,18,18,18,19,19,19,19,19,19,20,20,20,20,20,20,21,21,21,21,21,21,22,22,22,22,22,22,23,23,23,23,23,23,24,24,24,24,24,24,25,25,25,25,25,25,26,26,26,26,26,26,27,27,27,27,27,27,28,28,28,28,28,28,29,29,29,29,29,29,30,30,30,30,30,30,31,31,31,31,31,31,32,32,32,32,32,32,33,33,33,33,33,33,34,34,34,34,34,34,35,35,35,35,35,35,36,36,36,36,36,36,37,37,37,37,38,38,38,38,38,38,39,39,39,39,39,39,40,40,40,40,40,40,41,41,41,42,42,42,42,42,42,43,43,43,43,43,43,44,44,44,44,44,45,45,45,45,45,45,46,46,46,46,46,46,47,47,48,48,49,49,49,49,49,50,50,50,51,51,51,51,52,52,53,53,53,54,54,55,55,56,56,57,57,57,58,58,58,59,59,59,60,61,62,63,64,64,64,64,64,64,65,65,65,65,66,66,67,67,67,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,100,100,100]
+
 const initialData: any[] = [];
 const data1: any[] = [];
-let visits = 100;
+// let visits = 100;
+
 
 for (let i = 1; i < 366; i += 1) {
-    visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
-    initialData.push({date: new Date(2018, 0, i), value: visits});
+    // visits += Math.round((Math.random() < 0.5 ? 1 : -1) * Math.random() * 10);
+    initialData.push({date: new Date(2018, 0, i), value: data3[i]});
 }
 
-const positiveOnly = (value: number) => value > 0 ? value : 0;
+// const positiveOnly = (value: number) => value > 0 ? value : 0;
 
 const Marketers = () => {
+    console.dir(initialData);
     const chart = useRef(null);
     const [factor1, setFactor1] = useState(0);
     const [factor2, setFactor2] = useState(0);
@@ -29,17 +33,31 @@ const Marketers = () => {
 
     useEffect(() => {
         data1.length = 0;
+        const minValue = Math.min(...data3);
+        const maxValue = Math.max(...data3);
+        let currentValue: number;
         initialData.forEach((point, i) => {
+
+            if (initialData[i - factor1]) {
+                currentValue = initialData[i - factor1].value
+            } else
+            if (factor1 > 0 && !initialData[i - factor1]) {
+                currentValue = minValue;
+            } else
+            if (factor1 < 0 && !initialData[i - factor1]) {
+                currentValue = maxValue;
+            }
+
             data1.push({
                 date: point.date,
-                value: positiveOnly(point.value - factor1
-                    - (i > 0 ? (initialData[i-1].value + factor2)/2 : 0)
-                    - Math.ceil(factor3 / 2)
-                    + Math.ceil(factor4 * 1.5)
-                )
+                value: currentValue
             })
         })
+        // @ts-ignore
+        chart.current.validateData();
+        // eslint-disable-next-line react/destructuring-assignment
     }, [factor1, factor2, factor3, factor4])
+
 
     useLayoutEffect(() => {
         const x = am4core.create("chartdiv", am4charts.XYChart);
@@ -51,6 +69,12 @@ const Marketers = () => {
         dateAxis.renderer.grid.template.location = 0;
 
         const valueAxis = x.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.calculateTotals = true;
+        valueAxis.min = 0;
+        valueAxis.max = 110;
+        valueAxis.strictMinMax = true;
+        valueAxis.title.text = "Объём выполняемых работ, %";
+        valueAxis.title.fontWeight = "bold";
         // @ts-ignore
         valueAxis.tooltip.disabled = true;
         valueAxis.renderer.minWidth = 35;
@@ -71,7 +95,7 @@ const Marketers = () => {
         return () => {
             x.dispose();
         };
-    }, [factor1, factor2, factor3, factor4]);
+    }, []);
 
     return (
         <div>
@@ -83,11 +107,11 @@ const Marketers = () => {
             <Row style={{margin: '30px 45px'}}>
                 <Col span={12}>
                     <div style={{ width: '100%' }}>
-                        Фактор №1
+                        Смешение даты начала проекта
                     </div>
                     <div style={{ width: '80%', float: 'left' }}>
                         <Slider
-                            min={0}
+                            min={-100}
                             max={100}
                             onChange={setFactor1}
                             value={factor1}
@@ -96,7 +120,7 @@ const Marketers = () => {
                     </div>
                     <div style={{ width: '10%', float: 'left' }}>
                         <InputNumber
-                            min={0}
+                            min={-100}
                             max={100}
                             onVolumeChange={() => setFactor1}
                             value={factor1}
